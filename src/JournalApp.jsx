fdbf617d6657a273a41ea1115be5f630a9411bb7
FrancisHenriquez <francishenriquez07@gmail.com>
2022-11-17 16:09:51 -0400

    purple theme

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { LoginPage, RegisterPage } from "./auth/pages/index";
+import { AppTheme } from "./theme";
 
 export const JournalApp = () => {
   return (
+    <AppTheme>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>WA WA WA WA</div>} />
-
         {/* Registro y Login */}
         {/* <Route path='/auth/*' element={<AuthRoutes />}  />*/}
 
@@ -18,5 +18,6 @@ export const JournalApp = () => {
         {/* <Route path="/*" element={<JournalRoutes />} /> */}
       </Routes>
     </BrowserRouter>
+    </AppTheme>
   );
 };
