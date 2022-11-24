6f57c74536a390800c74390890a5015758c0701e
FrancisHenriquez <francishenriquez07@gmail.com>
2022-11-24 16:42:47 -0400

    journal UI

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,21 +1,27 @@
-import { Box } from "@mui/system";
-import { NavBar, SideBar } from "../components";
- 
-const drawerWith = 240;
+import { Toolbar } from '@mui/material';
+import { Box } from '@mui/system'
+import { NavBar, SideBar } from '../components';
+
+
+const drawerWidth = 280;
+
 export const JournalLayout = ({ children }) => {
   return (
-    <Box sx = {{ diplay: 'flex'}}>
+    <Box sx={{ display: 'flex' }}>
 
-        {/* Navbar */}
-        <NavBar drawerWith = { drawerWith } />
-        {/* Sidebar */}
-        <SideBar drawerWith={ drawerWith } />
-        <Box component='main' sx = {{ flexGrow: 1, p:3}} >
-            {/* Toolbar */}
+        <NavBar drawerWidth={ drawerWidth } />
+
+        <SideBar drawerWidth={ drawerWidth } />
+
+        <Box 
+            component='main'
+            sx={{ flexGrow: 1, p: 3 }}
+        >
+            <Toolbar />
+
             { children }
-
+            
         </Box>
-
     </Box>
   )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView } from "../views"
+import { NoteView, NothingSelectedView } from "../views"
 
 
 export const JournalPage = () => {
@@ -8,8 +8,8 @@ export const JournalPage = () => {
    <JournalLayout>
     {/* <Typography variant="body1" color="initial">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis natus repellat omnis culpa ipsum suscipit quam repellendus et rerum, odio possimus delectus voluptas rem, quo nam repudiandae dolor vero. Voluptate?</Typography>
      */}
-     <NothingSelectedView />
-     
+     {/* <NothingSelectedView /> */}
+     <NoteView />
    </JournalLayout>
   )
 }
